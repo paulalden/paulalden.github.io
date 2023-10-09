d254866833fe8d615d046d292c178f6c9788aaa4
Paul Danelli <prdanelli@gmail.com>
2023-10-09 21:32:58 +0100

    Empty out event callback

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,9 +1,7 @@
 document.addEventListener(
   "DOMContentLoaded",
   (event) => {
-    particlesJS.load("particles-js", "/assets/json/particles.json", function () {
-      console.log("particles.js loaded - callback");
-    });
+
   },
   false
 );
